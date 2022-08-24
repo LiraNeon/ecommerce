82f4143c514323bd39a72b60995a827c04d06e5a
LiraNeon <a01423502@itesm.mx>
2022-08-24 13:04:25 -0500

    Initial commit

---
+++ src/Interface/CartInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Repository;
+
+use App\Collection\Collection;
+use App\Model\Product;
+use Ramsey\Uuid\UuidInterface;
+
+interface CartInterface
+{
+    public function getProduct(int $id): ?Product;
+    
+    public function gettAllProducts(): Collection;
+    
+    public function addProduct(Product $product): void;
+
+    public function removeProduct(int $id): void;
+}
